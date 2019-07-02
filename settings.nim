73b0bcbd70b8825a782048990cc638f015563fd2
Skaruts <skaruts@users.noreply.github.com>
2019-07-02 04:58:53 +0100

    Update settings.nim

--- settings.nim
+++ settings.nim
@@ -2,9 +2,9 @@ import sktcod
 
 var
     show_fps*:bool = false
-    tile_use_faded_dark_color*:bool = true
-    tile_constant_dark_color*:Color = colorRGB(0, 32, 96)
-    tile_dark_factor*:float = 0.25
+    tile_use_faded_dark_color*:bool = true                  # use faded colors or single color outside fov
+    tile_constant_dark_color*:Color = colorRGB(0, 32, 96)   # color to use for tiles outside fov, if faded-color is off
+    tile_dark_factor*:float = 0.25                          # how much to darken the tiles' colors outside fov - 0 = fully faded, 1 = no fade
 
     use_fov*:bool = false
-    use_lighting:bool = true
+    # use_lighting:bool = true
